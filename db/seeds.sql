69ce3f015ef8894dd5d2b1a181470eaab447cbfd
Lisa McDonald <nixxxxer@aol.com>
2020-09-25 17:33:08 -0400

    Update schema and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 -- Insert a set of records.  write insert queries to populate the `burgers` table with at least three entries
-INSERT INTO burgers (burger_name) VALUES ('Avocado Burger');
-INSERT INTO burgers (burger_name) VALUES ('BLT Burger');
-INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
