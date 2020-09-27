55258dfab6ed18617081ee01252df6be8390a111
Lisa McDonald <nixxxxer@aol.com>
2020-09-27 13:23:06 -0400

    Update burger.js file

--- models/burger.js
+++ models/burger.js
@@ -1,44 +1,35 @@
-const orm = require("orm.js");
-
-// create the code that will call the ORM functions using burger specific input for the ORM
-
-selectAll();
-
-insertOne();
-
-updateOne();
-
-module.exports = burger;
-
-
-
-
-//Activity 16 orm.js
-
-
-
-
 // Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
+const orm = require("../config/orm.js");
 
-var cat = {
-  all: function(cb) {
-    orm.all("cats", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("cats", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("cats", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
+
+// Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+
+let burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+    // The variables cols and vals are arrays.
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+
+    deleteOne: function (condition, cb) {
+        orm.deleteOne("burgers", condition, function (res) {
+            cb(res);
+        });
+    }
 };
 
 // Export the database functions for the controller (catsController.js).
-module.exports = cat;
+module.exports = burger;
